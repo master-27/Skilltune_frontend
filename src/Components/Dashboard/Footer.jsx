a9692c9c2f62305f4ccd4ac97466d05ad0c1d9d1
MOHIT KUMAR SHARMA <117501886+master-27@users.noreply.github.com>
2025-02-14 17:51:43 +0530

    Update Footer.jsx

--- src/Components/Dashboard/Footer.jsx
+++ src/Components/Dashboard/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="text-center p-4 mt-6">
       <span className="text-500">Diksha </span><br></br>
-      <span className="text-blue-500"></span>
+      <span className="text-blue-500">dikshavashist2007@gmail.com</span>
     </footer>
   );
 };
